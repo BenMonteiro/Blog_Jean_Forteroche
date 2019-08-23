8921cd61a947ea566c8aeda8090c21d40c9a134c
Benjamin Monteiro Da Silva <benjamin.monteirodasilva@gmail.com>
2019-08-23 15:39:15 +0200

    Refactorisation du code

--- core/DefaultController.php
+++ core/DefaultController.php
@@ -2,11 +2,19 @@
 
 class DefaultController
 {
+    protected $content = '';
 
-    public function renderView(string $template = '', array $data = []): void
+    public function renderView($view, array $data = [], $template): void
     {
+        ob_start();
+        if (file_exists('src/views/'.$view.'.html')){
+            require_once 'src/views/'.$view.'.html';
+        }
+        $this->content = ob_get_contents();
         extract($data);
-        include $template;
+        if (file_exists($template)){
+            include $template;
+        }
     }
 
 }
--- src/controllers/ViewController.php
+++ src/controllers/ViewController.php
@@ -4,35 +4,35 @@ require_once 'core\DefaultController.php';
 
 class ViewController extends DefaultController
 {
+
+    private static $_template = 'src/views/template.php';
+
     public function displayHomePage()
     {
-        require_once 'src\views\Accueil.php';
-        $this->renderView('src\views\template.php', ['content' => $content]);
+
+        $this->renderView('Accueil', ['content' => $this->content, self::$_template]);
     }
 
     public function displayChapter1Page()
     {
-        require_once 'src\views\chapitres\chapitre1.php';
-        $this->renderView('src\views\template.php', ['content' => $content]);
+
+        $this->renderView('chapitre1', ['content' => $this->content, self::$_template]);
     }
 
     public function displayChapter2Page()
     {
-        require_once 'src\views\chapitres\chapitre2.php';
-        $this->renderView('src\views\template.php', ['content' => $content]);
+        $this->renderView('chapitre2', ['content' => $this->content, self::$_template]);
 
     }
 
     public function displayAuthorPage()
     {
-        require_once 'src\views\author.php';
-        $this->renderView('src\views\template.php', ['content' => $content]);
+        $this->renderView('author', ['content' => $this->content, self::$_template]);
     }
 
     public function displayContactPage()
     {
-        require_once 'src\views\contact.php';
-        $this->renderView('src\views\template.php', ['content' => $content]);
+        $this->renderView('contact', ['content' => $this->content, self::$_template]);
     }
 }
 
