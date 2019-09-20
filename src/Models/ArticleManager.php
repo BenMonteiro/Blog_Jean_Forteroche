fb90e35e037ebab38d9b617c2dfb1d30dcd72c29
Benjamin Monteiro Da Silva <benjamin.monteirodasilva@gmail.com>
2019-09-20 11:50:02 +0200

    Mise en marche de l'outil de mise à jour des articles

--- src/Models/ArticleManager.php
+++ src/Models/ArticleManager.php
@@ -28,23 +28,28 @@ class ArticleManager extends Manager
     /**
      * Update an existing article of the database
      */
-    public static function update($newTitle, $newContent, $newUser_id, $newDate, $id)
+    public static function update($imageURL, $imageDescription, $title, $content, $chapterDescription, $user_id, $id)
     {
-        $req = static::$bdd ->prepare(
-            'UPDATE article 
-            SET title = :newTitle, 
-            content = newContent, 
-            user_id = newUser_id, 
-            date_of_update = newDate
-            WHERE id = :id'
+        $req = static::$bdd->prepare(
+            'UPDATE article SET 
+                image_url = ?,
+                alt_image = ?,
+                title = ?,
+                content = ?,
+                chapter_description = ?,
+                user_id = ?,
+                date_of_update = NOW()
+            WHERE id = ?'
         );
 
         return $req->execute(array(
-            'newTitle' => $newTitle,
-            'newContent' => $newContent,
-            'newUser_id' => $newUser_id,
-            'newDate' => $newDate,
-            'id' => $id
+            $imageURL,
+            $imageDescription,
+            $title,
+            $content,
+            $chapterDescription,
+            $user_id,
+            $id
         ));
     }
 }
